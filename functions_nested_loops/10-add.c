c490fa47da49cdc4c403a3fde15e22b5e8421fad
umidcavad <umidcavadov0@gmail.com>
2024-10-27 19:51:33 +0000

    11th commit

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - check the code
+ * @number_1: - first number
+ * @number_2: - second number
+ * Return: Always 0.
+ */
+
+int add(int number_1, int number_2)
+{
+int answer;
+answer = number_1 + number_2;
+return (answer);
+return (0);
+}
