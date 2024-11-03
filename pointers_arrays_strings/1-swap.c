cfd0502ad04838fba0dcad29e92e536330f319d1
umidcavad <umidcavadov0@gmail.com>
2024-11-03 19:22:40 +0000

    second commit

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ * @a: - input of function
+ * @b: - input of function
+ */
+
+void swap_int(int *a, int *b)
+{
+int d;
+d = *a;
+*a = *b;
+*b = d;
+}
