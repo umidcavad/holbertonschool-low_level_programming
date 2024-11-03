e3f4309711cfffea6f2fe8d583f393453552a64b
umidcavad <umidcavadov0@gmail.com>
2024-11-03 18:45:06 +0000

    3rd  commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - checks if uppercase letter
+ * @a: - input of function
+ * @b: - input of function
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+int t;
+t = a * b;
+return (t);
+}
