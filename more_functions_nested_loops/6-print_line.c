3a93142ab704c5579c4e5dd2e6ce8603863782a5
umidcavad <umidcavadov0@gmail.com>
2024-11-03 19:10:29 +0000

    7th commit

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,17 @@
 #include "main.h"
-
 /**
- * more_numbers - prints number 0 to 14 10 times
+ * print_line - Main entry
+ * @n: variable
+ * Return: Printing line
  */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
 
-void more_numbers(void)
-{
-int n, t;
-for (t = 0; t < 10; t++)
-{
-for (n = 0; n <= 14; n++)
-{
-if (n >= 10)
-{
-_putchar((n / 10) + '0');
-}
-_putchar((n % 10) + '0');
-}
-_putchar('\n');
-}
+		for (i = 0; i < n; i++)
+		_putchar('_');
+	}
+	_putchar('\n');
 }
