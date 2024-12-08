2eabd30cc472a0aa093218f688ffa5052e77cd9d
umidcavad <umidcavadov0@gmail.com>
2024-12-08 19:10:27 +0000

    5th  commit

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,24 +1,16 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * get_dnodeint_at_index - returns the node with given index
+ * free_dlistint - frees all the nodes
  * @head: - input for given structure
- * @index: - index for list
- * Return: - return the node
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *index_node = head;
-	unsigned int i = 0;
+	dlistint_t *free_node = head;
 
-	while (i < index)
+	while (free_node)
 	{
-		index_node = index_node->next;
-		if (index_node == NULL)
-			break;
-		i++;
+		free(free_node);
+		free_node = free_node->next;
 	}
-	if (i < index)
-		return (NULL);
-	return (index_node);
 }
